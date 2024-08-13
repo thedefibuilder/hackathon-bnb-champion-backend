eebd955f9775a07c656836bd8c39b6f7d3aa4df6
urataps <alexeluca.spataru@gmail.com>
2024-08-13 17:10:21 +0300

    add vite starter and gnfd deploy endpoint

--- src/routes/GreenfieldRoutes.ts
+++ src/routes/GreenfieldRoutes.ts
@@ -1,9 +1,30 @@
 import { IReq, IRes } from "./common/types";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+
+const execAsync = promisify(exec);
 
 async function deploy(req: IReq, res: IRes) {
-  console.log("Deployu the mfg");
+  const { templateName, websiteName } = req.body;
 
-  return res.status(200).end();
+  console.log(`Deploying ${templateName}...`);
+
+  try {
+    await execAsync(
+      `cd templates/${templateName} && npm install && npm run build`
+    );
+
+    const { stdout } = await execAsync(
+      `./deploy_to_greenfield.sh templates/${templateName}/dist gnfd://barabula-website-test/${websiteName}`
+    );
+
+    console.log(stdout);
+
+    return res.status(200).end();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).end();
+  }
 }
 
 export default {
